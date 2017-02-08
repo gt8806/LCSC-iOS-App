b677c8c35cf520878e5eb4475ed34ff2e4de02e3
Janey Valerio <jlvalerio@lcmail.lcsc.edu>
2017-02-08 11:00:03 -0800

    New warrior wallet file

---
+++ campuslife/WarriorWallet.swift
@@ -0,0 +1,42 @@
+//
+//  WarriorWallet.swift
+//  LCSC
+//
+//  Created by Janey Valerio on 2/8/17.
+//  Copyright © 2017 LCSC. All rights reserved.
+//
+
+import UIKit
+
+class WarriorWallet: UITabBarController {
+
+	@IBOutlet weak var menuButton: UIBarButtonItem!
+	
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+		if self.revealViewController() != nil {
+			menuButton.target = self.revealViewController()
+			menuButton.action = "revealToggle:"
+			self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+		}
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
