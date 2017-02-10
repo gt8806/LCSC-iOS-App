021af2f19e9ae9cb6902e947dd1009d9eed453c2
Janey Valerio <jlvalerio@lcmail.lcsc.edu>
2017-02-10 11:28:13 -0800

    Warrior Wallet moved

--- campuslife/WarriorWallet.swift
+++ campuslife/WarriorWallet.swift
@@ -31,14 +31,42 @@ class WarriorWallet: UITabBarController {
         // Do any additional setup after loading the view.
     }
 	
+	//This gets the data from the database using the warriorWallet API
+	func get_data(key: String)-> (String, Int){
+		let session = URLSession.shared
+		var email = ""
+		var points = 0
+		points = 0
+		let warriorWalletRequestURL = URL(string: "")!// call to api
+		
+		let dataTask = session.dataTask(with: warriorWalletRequestURL, completionHandler: {
+			(data: Data?, response: URLResponse?, error: Error?) in
+			if let error = error {
+				print("Error:\n\(error)")
+			}
+			else {
+				do {
+					let mydata = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as! [String: AnyObject]
+					email = mydata["main"]!["email"]!! as! String
+					points = mydata["main"]!["points"]!! as! Int
+					
+				}
+				catch let jsonError as NSError {
+					print("JSON error description: \(jsonError.description)")
+				}
+			}
+		})
+		dataTask.resume()
+		return(email,points)
+	}
+	
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-		//menuButton.target = revealViewController()
-		//menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
-        // Dispose of any resources that can be recreated.
-    }
-    
+
 
     /*
     // MARK: - Navigation
