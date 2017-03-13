02f38d969c2f98111b64f4fb0dcc4ebb55c2a1b5
Gus Tropea <gus@Guss-MacBook-Air.local>
2017-03-13 11:19:25 -0700

    lots of changes

--- campuslife/ScriptWebView.swift
+++ campuslife/ScriptWebView.swift
@@ -12,7 +12,7 @@ class ScriptWebView: Authentication{
     
     fileprivate let warriorURL: String = "https://warriorwebss.lcsc.edu/Student/Account/Login?ReturnUrl=%2fStudent%2fPlanning%2fDegreePlans"
     fileprivate let blackboardURL: String = "https://lcsc.blackboard.com/"
-	fileprivate let warriorWalletURL: String = "http://isoptera.lcsc.edu/~jvalerio/"
+	fileprivate let warriorWalletURL: String = "http://isoptera.lcsc.edu:5199"
     
     override init() {
         super.init()
--- campuslife/WarriorWalletViewController.swift
+++ campuslife/WarriorWalletViewController.swift
@@ -27,32 +27,16 @@ class WarriorWalletViewController: UIViewController, UIWebViewDelegate {
 	
 	@IBOutlet weak var walletWebView: UIWebView!
 	
-	var urlValue = "http://isoptera.lcsc.edu/~jvalerio/"
+	var urlValue = "http://isoptera.lcsc.edu:5199"
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		
+		walletWebView.scalesPageToFit = true
 		if let url = URL (string: urlValue){
 			let requestObj = URLRequest(url: url)
 			_ = walletWebView.loadRequest(requestObj)
 		}
 		
-		var userName = ""
-		var passWord = ""
-		
-		if (defaults.string(forKey: "wwlogin") != nil) && (defaults.string(forKey: "wwpassword") != nil){
-			userName = defaults.string(forKey: "wwlogin")!
-			passWord = defaults.string(forKey: "wwpassword")!
-			//this is the case in which a user name and password exist, but they are not correct.
-			if login_worked(usrname: userName, password: passWord) == false {
-				print("place holder")
-			}
-			print("Username: "+userName)
-			print("password: "+passWord)
-		}
-		else{
-			print("placeholder")
-		}
 
 		
 		if self.revealViewController() != nil {
@@ -66,32 +50,7 @@ class WarriorWalletViewController: UIViewController, UIWebViewDelegate {
 		// Do any additional setup after loading the view.
 	}
 	
-	//This gets the data from the database using the warriorWallet API
-	func get_points(key: String)-> (Int){
-		let session = URLSession.shared
-		var points = 0
-		points = 0
-		let warriorWalletRequestURL = URL(string: "")!// call to api
-		
-		let dataTask = session.dataTask(with: warriorWalletRequestURL, completionHandler: {
-			(data: Data?, response: URLResponse?, error: Error?) in
-			if let error = error {
-				print("Error:\n\(error)")
-			}
-			else {
-				do {
-					let mydata = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as! [String: AnyObject]
-					points = mydata["main"]!["points"]!! as! Int
-					
-				}
-				catch let jsonError as NSError {
-					print("JSON error description: \(jsonError.description)")
-				}
-			}
-		})
-		dataTask.resume()
-		return(points)
-	}
+
 	
 	
 
